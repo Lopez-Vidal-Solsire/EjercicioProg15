013a92875c8e0f6a783626bfd412ff48cecb93e2
SOLSIRE <SOLSIRE@DESKTOP-1K97V9L>
2024-11-24 16:24:52 -0500

    Código Controlador

--- Lenguaje/src/main/java/Controlador/GestionLenguaje.java
+++ Lenguaje/src/main/java/Controlador/GestionLenguaje.java
@@ -4,13 +4,58 @@
 
 package Controlador;
 
+import Modelo.Lenguajeprog;
+
+
+
+
 /**
  *
  * @author SOLSIRE
  */
 public class GestionLenguaje {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static final int FACTOR_CRECIMIENTO=2;
+    private int contadorLenguajes=0;
+    private Lenguajeprog[] ArregloLenguajes=new Lenguajeprog[FACTOR_CRECIMIENTO];
+    
+    public void AgregarLenguaje( int AnioCreacion, String CaracteristicaPrin,String Nombre,String Utilización){
+            if (contadorLenguajes>=ArregloLenguajes.length){
+        Lenguajeprog[] ArrFaCrec=new Lenguajeprog[ArregloLenguajes.length+FACTOR_CRECIMIENTO];   
+        System.arraycopy(ArregloLenguajes,0,ArrFaCrec,0,ArregloLenguajes.length);
+        ArregloLenguajes=ArrFaCrec;
+        }else{
+                ArregloLenguajes[contadorLenguajes]=new Lenguajeprog(AnioCreacion, CaracteristicaPrin, Nombre, Utilización);
+                contadorLenguajes++;
+            }
+        }
+        
+    public Lenguajeprog buscarLenguaje(String Nombre) {
+        for (int i = 0; i < contadorLenguajes; i++) {
+            if (ArregloLenguajes[i].getNombre().equalsIgnoreCase(Nombre)) {
+                return ArregloLenguajes[i];
+            }
+        }
+        return null;
     }
+        public boolean EliminarLenguaje (String Nombre){
+        for (int i = 0; i < contadorLenguajes; i++) {
+            if (ArregloLenguajes[i].getNombre().equalsIgnoreCase(Nombre)) {
+                System.arraycopy(ArregloLenguajes, i + 1, ArregloLenguajes, i, contadorLenguajes - i - 1);
+                contadorLenguajes--;
+                return true;
+        }
+}
+    return false;
+    }
+
+    @Override
+    public String toString() {
+        return "GestionLenguaje{" + "ArregloLenguajes=" + ArregloLenguajes + '}';
+    }
+        
+public void ImprimirLenguaje(){
+    for (int i = 0; i < contadorLenguajes; i++) {
+        System.out.println(ArregloLenguajes[i].toString());
+    }
+}    
 }
