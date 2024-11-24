01d8a8963e91c45a464f3180b9907992bee8fae5
SOLSIRE <SOLSIRE@DESKTOP-1K97V9L>
2024-11-24 02:25:47 -0500

    Código modelo

--- Lenguaje/src/main/java/Modelo/Lenguajeprog.java
+++ Lenguaje/src/main/java/Modelo/Lenguajeprog.java
@@ -9,5 +9,54 @@ package Modelo;
  * @author SOLSIRE
  */
 public class Lenguajeprog {
+    private int AnioCreacion;
+    private String CaracteristicaPrin;
+    private String Nombre;
+    private String Utilización;
+
+    public Lenguajeprog(int AnioCreacion, String CaracteristicaPrin, String Nombre, String Utilización) {
+        this.AnioCreacion = AnioCreacion;
+        this.CaracteristicaPrin = CaracteristicaPrin;
+        this.Nombre = Nombre;
+        this.Utilización = Utilización;
+    }
+
+    
+    public int getAnioCreacion() {
+        return AnioCreacion;
+    }
+
+    public void setAnioCreacion(int AnioCreacion) {
+        this.AnioCreacion = AnioCreacion;
+    }
+
+    public String getCaracteristicaPrin() {
+        return CaracteristicaPrin;
+    }
+
+    public void setCaracteristicaPrin(String CaracteristicaPrin) {
+        this.CaracteristicaPrin = CaracteristicaPrin;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getUtilización() {
+        return Utilización;
+    }
+
+    public void setUtilización(String Utilización) {
+        this.Utilización = Utilización;
+    }
+
+    @Override
+    public String toString() {
+        return "Lenguajeprog{" + "AnioCreacion=" + AnioCreacion + ", CaracteristicaPrin=" + CaracteristicaPrin + ", Nombre=" + Nombre + ", Utilizaci\u00f3n=" + Utilización + '}';
+    }
     
 }
